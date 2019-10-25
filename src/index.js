95f4ace40156fb93fbfd2574e159a45cf872c2a2
tekken <tikaramthapamgr@gmail.com>
2019-10-25 11:59:00 +0545

    history setup and initial state

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import {createBrowserHistory} from "history";
+import {Router, Route, Switch} from "react-router-dom";
+// import App from "./App";
+// import * as serviceWorker from "./serviceWorker";
+import HomePage from "./components/pages/HomePage";
+import LoginPage from "./components/pages/LoginPage";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const hist = createBrowserHistory();
+
+ReactDOM.render(
+<Router history={hist}>
+    <Switch>
+        <Route path="/" exact component={HomePage}/>
+        <Route path="/login" exact component={LoginPage}/>
+    </Switch>
+</Router>, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// serviceWorker.unregister();
+
+// npm i history
